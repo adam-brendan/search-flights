556c8f5dd031cb75e6bfbda994c311fd08b83c53
Adam <adamcraveiro@gmail.com>
2019-07-16 15:27:36 -0400

    Create function to find matching date

--- searchflights.js
+++ searchflights.js
@@ -6,16 +6,32 @@ const searchFlights = () => {
 	const origin = process.argv[5];
 	const endpoint = `https://desktopapps.ryanair.com/v4/en-gb/availability?ADT=${adt}&CHD=0&DateOut=${departureDate}&Destination=${destination}&FlexDaysOut=2&INF=0&IncludeConnectingFlights=true&Origin=${origin}&RoundTrip=false&TEEN=0&ToUs=AGREED&exists=false`
 	const data = require("./data.json");
-	console.log("DATA BEFORE", data);
 	data.request.adt = adt;
 	data.request.departureDate = departureDate;
 	data.request.destination = destination;
 	data.request.origin = origin;
-	console.log("DATA AFTER", data)
 
 	request(endpoint, function(error, response, body) {
-		const jsonObj = JSON.parse(body)
-		console.log("BODY", jsonObj)
+		const flightData = JSON.parse(body)
+		console.log("FLIGHT DATA", flightData)
+		let flightNumber;
+		let departTime;
+		let arriveTime;
+		let price;
+		let currency = flightData.currency;
+		let datesArray = flightData.trips[0].dates;
+		const dateObj = () => {
+			let dateObj;
+			datesArray.forEach(function(date) {
+				if (date.dateOut.split("").slice(0, 10).join("") === departureDate) {
+					dateObj = date;
+				}
+			})
+			return dateObj;
+		}
+		console.log("DATE", dateObj())
+
+
 	})
 }
 
