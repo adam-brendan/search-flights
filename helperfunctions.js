d128f487439e356ddbcdf8fc4b07196d8ca5f8c4
Adam <adamcraveiro@gmail.com>
2019-07-17 00:38:54 -0400

    Refactor printFlights function, export logic to module

--- helperfunctions.js
+++ helperfunctions.js
@@ -7,6 +7,27 @@ module.exports = {
 			}
 		})
     return dateObj;
+	},
+	formatDate: (dateArray, timeArray) => {
+		let date = [];
+		let time = [];
+		if (dateArray[5] === "0" && dateArray[8] === "0") {
+			date.push(dateArray[6], "/", dateArray[9], "/", dateArray[0], dateArray[1], dateArray[2], dateArray[3])
+		} else if (dateArray[5] === "0") {
+			date.push(dateArray[6], "/", dateArray[8], dateArray[9], "/", dateArray[0], dateArray[1], dateArray[2], dateArray[3])
+		} else if (dateArray[8] === "0") {
+			date.push(dateArray[5], dateArray[6], "/", dateArray[9], "/", dateArray[0], dateArray[1], dateArray[2], dateArray[3])
+		} else {
+			date.push(dateArray[5], dateArray[6], "/", dateArray[8], dateArray[9], "/", dateArray[0], dateArray[1], dateArray[2], dateArray[3])
+		}
+
+		if (timeArray[0] === "0") {
+			time.push(timeArray[1], timeArray[2], timeArray[3], timeArray[4], timeArray[5], timeArray[6], timeArray[7])
+		} else {
+			time.push(timeArray[0], timeArray[1], timeArray[2], timeArray[3], timeArray[4], timeArray[5], timeArray[6], timeArray[7])
+		}
+
+		return (date.join("") + " " + time.join(""))
 	}
 }
 
--- searchflights.js
+++ searchflights.js
@@ -1,4 +1,4 @@
-const { filterDates } = require("./helperfunctions")
+const { filterDates, formatDate } = require("./helperfunctions")
 const request = require("request")
 
 const searchFlights = () => {
@@ -22,79 +22,38 @@ const searchFlights = () => {
 			return console.log("No flights found")
 		} else {
 			let datesArray = flightData.trips[0].dates;
-			const filteredDates = filterDates(datesArray, departureDate)
-			let flightsArray = filteredDates.flights;
+			const flightsArray = filterDates(datesArray, departureDate).flights
 
 			const printFlights = () => {
 				let availableFlights = [];
 				flightsArray.forEach(function(flight) {
 					let flightNumber = flight.flightNumber;
 					let price;
-					let depart;
-					let departureDate = [];
-					let departureTime = [];
-					let arrive;
-					let arrivalDate = [];
-					let arrivalTime = [];
 
 					if (flight.faresLeft !== 0) {
-
+						
 						// Departure date
-						let departureDateArray = flight.time[0].split("").splice(0, 10)
-						if (departureDateArray[5] === "0" && departureDateArray[8] === "0") {
-							departureDate.push(departureDateArray[6], "/", departureDateArray[9], "/", departureDateArray[0], departureDateArray[1], departureDateArray[2], departureDateArray[3])
-						} else if (departureDateArray[5] === "0") {
-							departureDate.push(departureDateArray[6], "/", departureDateArray[8], departureDateArray[9], "/", departureDateArray[0], departureDateArray[1], departureDateArray[2], departureDateArray[3])
-						} else if (departureDateArray[8] === "0") {
-							departureDate.push(departureDateArray[5], departureDateArray[6], "/", departureDateArray[9], "/", departureDateArray[0], departureDateArray[1], departureDateArray[2], departureDateArray[3])
-						} else {
-							departureDate.push(departureDateArray[5], departureDateArray[6], "/", departureDateArray[8], departureDateArray[9], "/", departureDateArray[0], departureDateArray[1], departureDateArray[2], departureDateArray[3])
-						}
-
-						// Departure time
-						let departureTimeArray = flight.time[0].split("").slice(11, 19)
-						if (departureTimeArray[0] === "0") {
-							departureTime.push(departureTimeArray[1], departureTimeArray[2], departureTimeArray[3], departureTimeArray[4], departureTimeArray[5], departureTimeArray[6], departureTimeArray[7])
-						} else {
-							departureTime.push(departureTimeArray[0], departureTimeArray[1], departureTimeArray[2], departureTimeArray[3], departureTimeArray[4], departureTimeArray[5], departureTimeArray[6], departureTimeArray[7])
-						}
-						depart = departureDate.join("") + " " + departureTime.join("")
-						// console.log("DEPART FINAL", depart)
+						let departureDateArray = flight.time[0].split("").splice(0, 10);
+						let departureTimeArray = flight.time[0].split("").slice(11, 19);
+						let depart = formatDate(departureDateArray, departureTimeArray);
 
 						// Arrival date
-						let arrivalDateArray = flight.time[1].split("").slice(0, 10)
-						if (arrivalDateArray[5] === "0" && arrivalDateArray[8] === "0") {
-							arrivalDate.push(arrivalDateArray[6], "/", arrivalDateArray[9], "/", arrivalDateArray[0], arrivalDateArray[1], arrivalDateArray[2], arrivalDateArray[3])
-						} else if (arrivalDateArray[5] === "0") {
-							arrivalDate.push(arrivalDateArray[6], "/", arrivalDateArray[8], arrivalDateArray[9], "/", arrivalDateArray[0], arrivalDateArray[1], arrivalDateArray[2], arrivalDateArray[3])
-						} else if (arrivalDateArray[8] === "0") {
-							arrivalDate.push(arrivalDateArray[5], arrivalDateArray[6], "/", arrivalDateArray[9], "/", arrivalDateArray[0], arrivalDateArray[1], arrivalDateArray[2], arrivalDateArray[3])
-						} else {
-							arrivalDate.push(arrivalDateArray[5], arrivalDateArray[6], "/", arrivalDateArray[8], arrivalDateArray[9], "/", arrivalDateArray[0], arrivalDateArray[1], arrivalDateArray[2], arrivalDateArray[3])
-						}
-
-						// Arrival time
-						let arrivalTimeArray = flight.time[1].split("").slice(11, 19)
-						if (arrivalTimeArray[0] === "0") {
-							arrivalTime.push(arrivalTimeArray[1], arrivalTimeArray[2], arrivalTimeArray[3], arrivalTimeArray[4], arrivalTimeArray[5], arrivalTimeArray[6], arrivalTimeArray[7])
-						} else {
-							arrivalTime.push(arrivalTimeArray[0], arrivalTimeArray[1], arrivalTimeArray[2], arrivalTimeArray[3], arrivalTimeArray[4], arrivalTimeArray[5], arrivalTimeArray[6], arrivalTimeArray[7])
-						}
-
-						arrive = arrivalDate.join("") + " " + arrivalTime.join("")
-						// console.log("ARRIVE FINAL", arrive)
+						let arrivalDateArray = flight.time[1].split("").slice(0, 10);
+						let arrivalTimeArray = flight.time[1].split("").slice(11, 19);
+						let arrive = formatDate(arrivalDateArray, arrivalTimeArray);
 
 						price = flight.regularFare.fares[0].amount;
-						// console.log(`FARES FOR FLIGHT ${flightNumber}: ${price}`)
 
 						availableFlights.push(`${flightNumber} ${origin} --> ${destination} (${depart} --> ${arrive}) - ${price} ${currency}`)
 					} 
 				})
+				
 				if (availableFlights === undefined || availableFlights.length === 0) {
 					return "No flights available"
 				} else {
 					return availableFlights;
 				}
+				
 			}
 			printFlights().forEach(function(flight) {
 				return console.log(flight);
